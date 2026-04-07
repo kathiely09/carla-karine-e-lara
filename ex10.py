c6f0118591c468ff74a7032791f3fc3e95d34c12
Carla <santos.souza.carla@escola.pr.gov.br>
2026-04-07 16:08:15 -0300

    Add comparison of two input numbers

---
+++ ex10.py
@@ -0,0 +1,9 @@
+n1=int(input("Digite o 1º número: "))
+n2= int(input("Digite o 2º número: "))
+
+if n1 > n2:
+    print("0 1° número é maior")
+
+else:
+    print("0 2° número é maior ou igual")
+    
