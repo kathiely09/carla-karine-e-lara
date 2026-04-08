b5499621fff44bceb5f3f741e30837d5f2a2e76f
Carla <santos.souza.carla@escola.pr.gov.br>
2026-04-08 07:10:27 -0300

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# Exercício 12 Número par ou impar
+numero= int(input("Digite um número: "))
+
+if numero > 0:
+    print("Número positivo")
+elif numero == 0:
+    print("Número é zero")
+else:
+    print("Número negativo")
